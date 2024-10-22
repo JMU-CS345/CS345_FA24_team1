7cb50207b6bff01c78772a59141345e4e17aa460
jay6o <richforeverrr6@proton.me>
2024-10-22 17:58:22 -0400

    restored so that everything is handled by Arena

--- sketch.js
+++ sketch.js
@@ -3,20 +3,12 @@
  */
 
 let arena;
-let ui;
-let player;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    // arena = new Arena();
-    player = {health:5}; // just for now, while player class isn't finished
-    ui = new UI(player);
-    ui.startTime();
+    // arena = new Arena(); // not implemented
 }
 
 function draw() {
-    background(255);
-    ui.draw();
     // arena.update();
     // arena.draw();
 }
