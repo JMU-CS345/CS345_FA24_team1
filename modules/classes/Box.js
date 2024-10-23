129702a55fd05a58365e89e211cd324d5487de21
Joey Pabalan <pabalajb@dukes.jmu.edu>
2024-10-23 16:55:55 -0400

    Box range checking and intersects() method

--- modules/classes/Box.js
+++ modules/classes/Box.js
@@ -3,10 +3,28 @@
  */
 class Box {
     constructor(x, y, width, height) {
+        // Check width and height non-negative
+        if ((width<0) || (height<0) || !isFinite(width) || !isFinite(height))
+            throw new RangeError(
+                    "width and height must be non-negative finite numbers");
+
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+    }
+
+    /* true if this Box overlaps with Box other, false otherwise */
+    intersects(other) {
+        let tx1 = this.x, ty1 = this.y,
+            tx2 = this.x + this.width, ty2 = this.y + this.height,
+            ox1 = other.x, oy1 = other.y,
+            ox2 = other.x + other.width, oy2 = other.y + other.height;
+
+        // https://stackoverflow.com/questions/306316/determine-if-two-rectangles-overlap-each-other
+        // No intersection if this Box is completely above, below, left, or
+        // right of the other Box. Intesection if none of those are true.
+        return !((ty2 < oy1) || (ty1 > oy2) || (tx2 < ox1) || (tx1 > ox2));
     }
 }
 
