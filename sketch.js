8524af92ba8e5f702bfca0cae00530c198f65900
Joey Pabalan <pabalajb@dukes.jmu.edu>
2024-10-30 09:34:13 -0400

    create canvas in sketch

--- sketch.js
+++ sketch.js
@@ -5,8 +5,14 @@
 let arena, ui;
 
 function preload() {
+    // Create Arena and UI in preload() to allow for image loading to finish
     arena = new Arena();
     ui = new UI(arena);
+}
+
+function setup() {
+    // Canvas must be created no earlier than the start of setup()
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
