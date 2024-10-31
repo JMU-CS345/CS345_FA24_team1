1bdb00b19fe9ca1f386d7e7170797ec13e9fca8c
TylerAvery <tyleravery65@gmail.com>
2024-10-30 22:55:39 -0400

    Weapon json and Weapon class unifinished

--- modules/classes/Weapon.js
+++ modules/classes/Weapon.js
@@ -1,15 +1,11 @@
-const WeaponType = Object.freeze ({
-    MELEE: 'melee',
-    RANGED: 'ranged'
-
-});
-
 class Weapon {
 
-    constructor (type, damage, fireRate) {
-        this.type = type; // WeaponType
+    constructor (weapon, damage, fireRate, sprite, player) {
+        this.weapon = weapon; // Weapon
         this.damage = damage; // Damage dealt by the weapon (double)
         this.fireRate = fireRate; // Hits per second (double)
+        this.sprite = sprite; // The sprite for the weapon
+        this.player = player;
         this.lastFireTime = 0; // Timestamp of the last fire action (long)
     }
 
@@ -20,11 +16,29 @@ class Weapon {
         // Check if the weapon can fire again based on fire rate
         if (timeSinceLastFire >= (1000 / this.fireRate)) {
             this.lastFireTime = currentTime; // Update last fire time
+
+        //further implementation of fire
         }
     }
 
-    switchWeapon() {
+    switch_Weapon(newWeapon, newWeaponSprite) {
         //switches weapon equipped
+        this.weapon = newWeapon;
+        change_sprite(newWeaponSprite);
+        draw();
+    }
+
+    change_sprite(newWeaponSprite) {
+        //can be called by player if the player changes direction or if player switches weapons
+        this.sprite = newWeaponSprite;
+    }
+
+    get_sprite() {
+        return this.sprite;
+    }
+
+    get_current_weapon() {
+        return this.weapon;
     }
 
     get_damage() {
@@ -37,5 +51,11 @@ class Weapon {
 
     get_fireRate() {
         return this.fireRate;
+    }
+
+    draw() {
+        //draws the sprite for the current weapon
+        //would need to call the current players location
+        //image(sprite, player.location.x, player.location.y)
     }
 }
