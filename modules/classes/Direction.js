b9c77811b43a0c58b9d7172f6ee96469d18c5136
Joey Pabalan <pabalajb@dukes.jmu.edu>
2024-11-05 14:57:28 -0500

    add Direction.degrees() and Direction.radians()

--- modules/classes/Direction.js
+++ modules/classes/Direction.js
@@ -7,6 +7,18 @@ const Direction = {
     UP: 2,
     DOWN: 3,
 
+    /* Returns the angle of a Direction in degrees. */
+    degrees: function(direction) {
+        const degs = [180, 0, 270, 90];
+        return degs[direction];
+    },
+
+    /* Returns the angle of a Direction in radians. */
+    radians: function(direction) {
+        const rads = [PI, 0.0, PI+HALF_PI, HALF_PI];
+        return rads[direction];
+    },
+
     /* Returns a Direction of the most important change in direction between
      * Vector2Ds start to end. Horizontal takes priority over vertical. */
     primaryDirectionChange: function(start, end) {
