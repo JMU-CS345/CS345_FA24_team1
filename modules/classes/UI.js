e15a8f4718912b55c3974eff11755c0bce4eb421
jay6o <richforeverrr6@proton.me>
2024-11-30 13:54:21 -0500

    removed debugging print statement

--- modules/classes/UI.js
+++ modules/classes/UI.js
@@ -55,7 +55,6 @@ class UI {
 
         if (previousWeaponIndex !== this.selectedWeaponIndex) {
             const weaponKeys = ["katana", "pistol", "shotgun", "rifle", "rocket"];
-            console.log(this.arena.getPlayer().weapons);
             const newWeapon = weaponKeys[this.selectedWeaponIndex];
             this.arena.getPlayer().sprite = assets.playersprites[newWeapon];
             let hasWeapon = this.arena.getPlayer().switchWeapon(this.selectedWeaponIndex); // check if player has weapon
